f3423cbd4159d8077fcb0be5f5863aa3b2addc10
Vlad Orzhekhovskiy <vlad@uralian.com>
2015-02-18 10:57:15 -0500

    SelectValues and AddChecksum implemented

---
+++ src/main/scala/com/ignition/samples/SimpleFlow.scala
@@ -0,0 +1,37 @@
+package com.ignition.samples
+
+import org.apache.spark.SparkContext
+import org.joda.time.DateTime
+import org.slf4j.LoggerFactory
+import com.eaio.uuid.UUID
+import com.ignition.data._
+import com.ignition.workflow.rdd.grid.{ AddChecksum, DigestAlgorithm, SelectValues }
+import com.ignition.workflow.rdd.grid.input.DataGridInput
+
+object SimpleFlow extends App {
+  val log = LoggerFactory.getLogger(getClass)
+
+  implicit val sc = new SparkContext("local[4]", "test")
+
+  val meta = uuid("id") ~ string("name") ~ int("weight") ~ datetime("dob")
+
+  val grid = DataGridInput(meta).
+    addRow(new UUID, "john", 155, dob(1980, 5, 2)).
+    addRow(new UUID, "jane", 190, dob(1982, 4, 25)).
+    addRow(new UUID, "jake", 160, dob(1974, 11, 3)).
+    addRow(new UUID, "josh", 120, dob(1995, 1, 10))
+
+  val chksum = AddChecksum("chk", DigestAlgorithm.SHA256, List("name", "weight"))
+
+  val select = SelectValues.create.retype[String]("dob").retain("id", "dob", "chk")
+
+  grid.connectTo(chksum).connectTo(select)
+
+  println("Output meta: " + select.outMetaData)
+  println("Output data:")
+  select.output foreach println
+
+  sc.stop
+
+  def dob(year: Int, month: Int, day: Int) = new DateTime(year, month, day, 0, 0)
+}
--- src/main/scala/com/ignition/workflow/rdd/grid/entities.scala
+++ src/main/scala/com/ignition/workflow/rdd/grid/entities.scala
@@ -1,12 +1,11 @@
 package com.ignition.workflow.rdd.grid
 
 import scala.xml.{ Elem, Node }
-
 import org.apache.spark.SparkContext
 import org.apache.spark.rdd.RDD
-
 import com.ignition.data.{ DataRow, RowMetaData }
 import com.ignition.workflow.Step0
+import com.ignition.workflow.Step1
 
 /**
  * The base step of the ignition grid framework.
@@ -40,6 +39,20 @@ trait GridStep0 extends Step0[RDD[DataRow], SparkContext] with GridStep {
   protected def computeRDD(implicit sc: SparkContext): RDD[DataRow]
 
   protected def compute(sc: SparkContext): RDD[DataRow] = computeRDD(sc)
+}
+
+/**
+ * Grid step with one input.
+ */
+trait GridStep1 extends Step1[RDD[DataRow], RDD[DataRow], SparkContext] with GridStep {
+
+  protected def computeRDD(rdd: RDD[DataRow]): RDD[DataRow]
+
+  protected def compute(sc: SparkContext)(rdd: RDD[DataRow]): RDD[DataRow] = computeRDD(rdd)
+
+  protected def inMetaData: Option[RowMetaData] = in flatMap {
+    _.asInstanceOf[GridStep].outMetaData
+  }
 }
 
 /**
