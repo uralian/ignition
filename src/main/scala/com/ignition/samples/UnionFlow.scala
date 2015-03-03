6fd24fcaefa2c842acc2121259fd2abca2815770
Vlad Orzhekhovskiy <vlad@uralian.com>
2015-03-03 00:12:05 -0500

    TextFileOutput step implemented

--- src/main/scala/com/ignition/samples/UnionFlow.scala
+++ src/main/scala/com/ignition/samples/UnionFlow.scala
@@ -3,11 +3,12 @@ package com.ignition.samples
 import org.apache.spark.SparkContext
 import org.joda.time.DateTime
 import org.slf4j.LoggerFactory
+
 import com.eaio.uuid.UUID
-import com.ignition.data._
-import com.ignition.workflow.rdd.grid._
-import com.ignition.workflow.rdd.grid.input._
-import com.ignition.workflow.rdd.grid.output.DebugOutput
+import com.ignition.data.{ columnInfo2metaData, datetime, int, string, uuid }
+import com.ignition.workflow.rdd.grid.Union
+import com.ignition.workflow.rdd.grid.input.DataGridInput
+import com.ignition.workflow.rdd.grid.output.{ DebugOutput, TextFileOutput }
 
 object UnionFlow extends App {
 
@@ -16,12 +17,12 @@ object UnionFlow extends App {
   implicit val sc = new SparkContext("local[4]", "test")
 
   val meta = uuid("id") ~ string("name") ~ int("weight") ~ datetime("dob")
-    
+
   val grid1 = DataGridInput(meta).
     addRow(new UUID, "john", 155, dob(1980, 5, 2)).
     addRow(new UUID, "jake", 160, dob(1974, 11, 3)).
     addRow(new UUID, "josh", 120, dob(1995, 1, 10))
-    
+
   val grid2 = DataGridInput(meta).
     addRow(new UUID, "jane", 190, dob(1982, 4, 25)).
     addRow(new UUID, "jake", 160, dob(1974, 11, 3)).
@@ -29,16 +30,19 @@ object UnionFlow extends App {
 
   val grid3 = DataGridInput(meta).
     addRow(new UUID, "jess", 155, dob(1980, 5, 2))
-    
+
   val union = Union()
   union.connectFrom(grid1)
   union.connectFrom(grid2)
   union.connectFrom(grid3)
-  
+
   val debug = DebugOutput()
   union.connectTo(debug).output
-  
+
+  val csv = TextFileOutput("union_output.csv", "name" -> "%-10s", "weight" -> "%6d", "dob" -> "%s")
+  union.connectTo(csv).output
+
   sc.stop
-  
+
   def dob(year: Int, month: Int, day: Int) = new DateTime(year, month, day, 0, 0)
 }
