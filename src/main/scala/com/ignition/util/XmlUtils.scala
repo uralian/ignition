c4b1f53fada1a87739c1c7bb94e47a1b923c0acd
Vlad Orzhekhovskiy <vlad@uralian.com>
2015-03-18 13:13:16 -0400

    asDouble/getAsDouble methods added to XmlUtils

--- src/main/scala/com/ignition/util/XmlUtils.scala
+++ src/main/scala/com/ignition/util/XmlUtils.scala
@@ -23,12 +23,16 @@ object XmlUtils {
 
     def asInt: Int = xml.head
     def getAsInt: Option[Int] = nodeSeqToOpt(xml)
+    
+    def asDouble: Double = xml.head
+    def getAsDouble: Option[Double] = nodeSeqToOpt(xml)
 
     def asBoolean: Boolean = xml.head
     def getAsBoolean: Option[Boolean] = nodeSeqToOpt(xml)
 
     implicit private def nodeToString(xml: Node): String = xml.text
     implicit private def nodeToInt(xml: Node): Int = xml.text.toInt
+    implicit private def nodeToDouble(xml: Node): Double = xml.text.toDouble
     implicit private def nodeToBoolean(xml: Node): Boolean = xml.text.toBoolean
 
     private def nodeSeqToOpt[T](xml: NodeSeq)(implicit cnv: Node => T) = xml.headOption map cnv
