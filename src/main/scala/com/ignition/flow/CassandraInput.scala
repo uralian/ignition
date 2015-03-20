485b408e1176c258e553c5acf5fd8dbe4fa17e68
Vlad Orzhekhovskiy <vlad@uralian.com>
2015-03-20 14:21:00 -0400

    CassandraInput step implemented

---
+++ src/main/scala/com/ignition/flow/CassandraInput.scala
@@ -0,0 +1,111 @@
+package com.ignition.flow
+
+import java.sql.Date
+
+import scala.Array.canBuildFrom
+import scala.xml.{ Elem, Node }
+import scala.xml.NodeSeq.seqToNodeSeq
+
+import org.apache.spark.annotation.DeveloperApi
+import org.apache.spark.sql.{ DataFrame, Row, SQLContext }
+import org.apache.spark.sql.types._
+
+import com.datastax.spark.connector.{ CassandraRow, toSparkContextFunctions }
+import com.datastax.spark.connector.types.TypeConverter
+import com.ignition.types.TypeUtils.{ typeForName, typeForValue, valueToXml, xmlToValue }
+import com.ignition.util.XmlUtils.RichNodeSeq
+
+/**
+ * CQL WHERE clause.
+ */
+case class Where(cql: String, values: Any*) extends XmlExport {
+  def toXml: Elem =
+    <where>
+      <cql>{ cql }</cql>
+      { values map (v => <arg type={ typeForValue(v).typeName }>{ valueToXml(v) }</arg>) }
+    </where>
+}
+
+/**
+ * CQL WHERE companion object.
+ */
+object Where {
+  /**
+   * Converts XML into a WHERE clause.
+   */
+  def fromXml(xml: Node) = {
+    val cql = (xml \ "cql").text
+    val values = (xml \ "arg") map { node =>
+      val dataType = typeForName((node \ "@type").asString)
+      xmlToValue(dataType, node.child)
+    }
+    Where(cql, values: _*)
+  }
+}
+
+/**
+ * Reads rows from Apache Cassandra.
+ *
+ * @author Vlad Orzhekhovskiy
+ */
+case class CassandraInput(keyspace: String, table: String, schema: StructType,
+  where: Option[Where]) extends Producer with XmlExport {
+
+  protected def compute(implicit ctx: SQLContext): DataFrame = {
+    val schema = this.schema
+    val dataTypes = schema.fields map (_.dataType)
+    val cassRDD = ctx.sparkContext.cassandraTable[CassandraRow](keyspace, table).select(schema.fieldNames: _*)
+    val rows = where map (w => cassRDD.where(w.cql, w.values: _*)) getOrElse cassRDD
+    val rdd = rows map { cr =>
+      @inline def convert[T](x: Any)(implicit tc: TypeConverter[T]) = tc.convert(x)
+      val data = dataTypes zip cr.iterator.toArray[Any] map {
+        case (BinaryType, x) => convert[Array[Byte]](x)
+        case (BooleanType, x) => convert[Boolean](x)
+        case (StringType, x) => convert[String](x)
+        case (ByteType, x) => convert[Byte](x)
+        case (ShortType, x) => convert[Short](x)
+        case (IntegerType, x) => convert[Int](x)
+        case (LongType, x) => convert[Long](x)
+        case (FloatType, x) => convert[Float](x)
+        case (DoubleType, x) => convert[Double](x)
+        case (DecimalType.Unlimited, x) => Decimal(convert[BigDecimal](x))
+        case (DateType, x) => new java.sql.Date(convert[Date](x).getTime)
+        case (TimestampType, x) => new java.sql.Timestamp(convert[Date](x).getTime)
+        case (t @ _, _) => throw new IllegalArgumentException(s"Invalid data type: $t")
+      }
+      Row.fromSeq(data)
+    }
+    ctx.createDataFrame(rdd, schema)
+  }
+
+  def outputSchema: Option[StructType] = Some(schema)
+
+  def toXml: Elem =
+    <cassandra-input keyspace={ keyspace } table={ table }>
+      { DataGrid.schemaToXml(schema) }
+      {
+        where map (_.toXml) toList
+      }
+    </cassandra-input>
+
+  private def writeObject(out: java.io.ObjectOutputStream): Unit = unserializable
+}
+
+/**
+ * Cassandra Input companion object.
+ */
+object CassandraInput {
+  def apply(keyspace: String, table: String, schema: StructType): CassandraInput =
+    apply(keyspace, table, schema, None)
+
+  def apply(keyspace: String, table: String, schema: StructType, cql: String, args: Any*): CassandraInput =
+    apply(keyspace, table, schema, Some(Where(cql, args: _*)))
+
+  def fromXml(xml: Node) = {
+    val keyspace = (xml \ "@keyspace").text
+    val table = (xml \ "@table").text
+    val meta = DataGrid.xmlToSchema((xml \ "schema").head)
+    val where = (xml \ "where").headOption map Where.fromXml
+    CassandraInput(keyspace, table, meta, where)
+  }
+}
