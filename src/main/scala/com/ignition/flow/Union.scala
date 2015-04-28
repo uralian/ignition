cf7820658c9aa1e79975e3d8fd2ae3c3159ab5cb
Vlad Orzhekhovskiy <vlad@uralian.com>
2015-04-28 00:28:38 -0400

    Union step implemented

---
+++ src/main/scala/com/ignition/flow/Union.scala
@@ -0,0 +1,35 @@
+package com.ignition.flow
+
+import org.apache.spark.sql._
+import org.apache.spark.sql.types._
+
+/**
+ * Merges multiple DataFrames. All of them must have identical schema.
+ *
+ * @author Vlad Orzhekhovskiy
+ */
+case class Union() extends Merger(Union.MAX_INPUTS) {
+  
+  override val allInputsRequired = false
+
+  protected def compute(args: Array[DataFrame], limit: Option[Int])(implicit ctx: SQLContext): DataFrame = {
+    val rdd = ctx.sparkContext.union(args filter (_ != null) map (_.rdd))
+    val df = ctx.createDataFrame(rdd, outSchema(0))
+    limit map df.limit getOrElse df
+  }
+
+  protected def computeSchema(inSchemas: Array[StructType])(implicit ctx: SQLContext): StructType = {
+    val schemas = inSchemas.filter(_ != null)
+    assert(schemas.tail.forall(_ == schemas.head), "Input schemas do not match")
+    inSchemas.head
+  }
+  
+  private def writeObject(out: java.io.ObjectOutputStream): Unit = unserializable
+}
+
+/**
+ * Union companion object.
+ */
+object Union {
+  val MAX_INPUTS = 10
+}
