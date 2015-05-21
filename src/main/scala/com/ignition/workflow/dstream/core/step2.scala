c720514b15e05053178615a53d7d7234be981b7f
Vlad Orzhekhovskiy <vlad@uralian.com>
2015-05-21 16:07:01 -0400

    Old DStream classes removed from codebase

--- src/main/scala/com/ignition/workflow/dstream/core/step2.scala
+++
@@ -1,9 +0,0 @@
-package com.ignition.workflow.dstream.core
-
-import scala.reflect.ClassTag
-
-import org.apache.spark.streaming.StreamingContext.toPairDStreamFunctions
-
-import com.ignition.workflow.dstream.DStreamStep2
-
-class DStreamJoin[K: ClassTag, V: ClassTag, W: ClassTag] extends DStreamStep2[(K, V), (K, W), (K, (V, W))](_ join _)
