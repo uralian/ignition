1e41709fd65df58b4702321594a6c0e8e0fb0a2f
Vlad Orzhekhovskiy <vlad@uralian.com>
2015-07-31 15:43:22 -0400

    stream KafkaInput properties added

--- src/main/scala/com/ignition/stream/KafkaInput.scala
+++ src/main/scala/com/ignition/stream/KafkaInput.scala
@@ -15,17 +15,20 @@ import kafka.serializer.StringDecoder
  *
  * @author Vlad Orzhekhovskiy
  */
-case class KafkaInput(brokers: Iterable[String], topics: Iterable[String], field: String = "payload") extends StreamProducer {
+case class KafkaInput(brokers: Iterable[String], topics: Iterable[String],
+                      kafkaProperties: Map[String, String] = Map.empty, field: String = "payload") extends StreamProducer {
 
   def brokers(hosts: String*): KafkaInput = copy(brokers = hosts)
   def brokers(hosts: String): KafkaInput = copy(brokers = hosts.split(",\\s*"))
 
   def topics(t: String*): KafkaInput = copy(topics = t)
-  def topics(t: String): KafkaInput = copy(topics = t.split(",\\s"))
+  def topics(t: String): KafkaInput = copy(topics = t.split(",\\s*"))
+
+  def properties(prop: (String, String)*): KafkaInput = copy(kafkaProperties = prop.toMap)
 
   val schema = string(field)
 
-  private val kafkaParams = Map("metadata.broker.list" -> brokers.mkString(","))
+  private val kafkaParams = Map("metadata.broker.list" -> brokers.mkString(",")) ++ kafkaProperties
 
   protected def compute(limit: Option[Int])(implicit runtime: SparkRuntime): DataStream = {
     val raw = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](ssc, kafkaParams, topics.toSet)
