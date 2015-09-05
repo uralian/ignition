22899d25e3df6d9fcd7d45547f6715e1eb6d8d8e
Vlad Orzhekhovskiy <vlad@uralian.com>
2015-09-05 01:01:12 -0400

    DebugOutput - toXml/fromXml, toJson/fromJson implemented

--- src/main/scala/com/ignition/frame/DebugOutput.scala
+++ src/main/scala/com/ignition/frame/DebugOutput.scala
@@ -1,28 +1,42 @@
 package com.ignition.frame
 
 import scala.xml.{ Elem, Node }
+import org.json4s.JValue
+import org.json4s.JsonDSL._
+import org.json4s.jvalue2monadic
 
-import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql._
 import org.apache.spark.sql.types._
-
-import com.ignition.{ SparkRuntime, XmlExport }
-import com.ignition.util.XmlUtils.{ RichNodeSeq, booleanToText }
+import com.ignition.SparkRuntime
+import com.ignition.util.XmlUtils.{ RichNodeSeq, booleanToText, intToText, optToOptText }
+import com.ignition.util.JsonUtils._
+import scala.annotation.tailrec
 
 /**
  * Prints out data on the standard output.
  *
  * @author Vlad Orzhekhovskiy
  */
-case class DebugOutput(names: Boolean = true, types: Boolean = false, header: Option[String] = None)
-  extends FrameTransformer with XmlExport {
-  
-  def names(names: Boolean): DebugOutput = copy(names = names)
-  def types(types: Boolean): DebugOutput = copy(types = types)
-  def header(header: String): DebugOutput = copy(header = Some(header))
+case class DebugOutput(names: Boolean = true, types: Boolean = false,
+                       title: Option[String] = None, maxWidth: Option[Int] = Some(80))
+  extends FrameTransformer {
+
+  import DebugOutput._
+
+  def showNames(names: Boolean): DebugOutput = copy(names = names)
+  def showTypes(types: Boolean): DebugOutput = copy(types = types)
+
+  def title(title: String): DebugOutput = copy(title = Some(title))
+  def noTitle(): DebugOutput = copy(title = None)
+
+  def maxWidth(width: Int): DebugOutput = copy(maxWidth = Some(width))
+  def unlimitedWidth(): DebugOutput = copy(maxWidth = None)
 
   protected def compute(arg: DataFrame, limit: Option[Int])(implicit runtime: SparkRuntime): DataFrame = {
     val schema = arg.schema
-    val widths = calculateWidths(schema)
+    val data = limit map arg.take getOrElse arg.collect
+
+    val widths = calculateWidths(schema, data)
     val delimiter = widths map ("-" * _) mkString ("+", "+", "+")
 
     val dataTypes = schema map (_.dataType)
@@ -31,8 +45,8 @@ case class DebugOutput(names: Boolean = true, types: Boolean = false, header: Op
       case (dt, width) => s"%${width}s".format(dt.typeName)
     } mkString ("|", "|", "|")
 
-    header foreach println
-    
+    title foreach println
+
     if (names || types) println(delimiter)
 
     if (names) {
@@ -46,8 +60,6 @@ case class DebugOutput(names: Boolean = true, types: Boolean = false, header: Op
       println(typeLine)
 
     println(delimiter)
-
-    val data = limit map arg.take getOrElse arg.collect
 
     data foreach { row =>
       val str = (dataTypes zip row.toSeq zip widths) map {
@@ -72,10 +84,35 @@ case class DebugOutput(names: Boolean = true, types: Boolean = false, header: Op
 
   protected def computeSchema(inSchema: StructType)(implicit runtime: SparkRuntime): StructType = inSchema
 
-  def toXml: Elem = <debug-output names={ names } types={ types }/>
+  def toXml: Elem =
+    <node names={ names } types={ types } max-width={ maxWidth }>
+      { title map (t => <title>{ t }</title>) toList }
+    </node>.copy(label = tag)
 
-  private def calculateWidths(schema: StructType) = schema.fieldNames map { name =>
-    math.max(math.min(name.length, 15), 10)
+  def toJson: JValue = ("tag" -> tag) ~ ("names" -> names) ~ ("types" -> types) ~
+    ("maxWidth" -> maxWidth) ~ ("title" -> title)
+
+  private def calculateWidths(schema: StructType, data: Array[Row]) = {
+
+    @tailrec
+    def normalize(widths: Seq[Int]): Seq[Int] = {
+      val total = widths.sum + widths.size + 1
+      if (total > maxWidth.getOrElse(0))
+        normalize(widths.map(_ * 9 / 10))
+      else
+        widths
+    }
+
+    val widths = (schema zipWithIndex) map {
+      case (field, idx) =>
+        val nameWidth = if (names) field.name.size else 0
+        val typeWidth = if (types) field.dataType.typeName.size else 0
+        val valueWidths = data map { row => Option(row(idx)) map (_.toString.size) getOrElse 0 }
+        val maxValueWidth = valueWidths.max
+        List(nameWidth, typeWidth, maxValueWidth, 2).max
+    }
+
+    normalize(widths)
   }
 }
 
@@ -83,9 +120,23 @@ case class DebugOutput(names: Boolean = true, types: Boolean = false, header: Op
  * Debug output companion object.
  */
 object DebugOutput {
+  val tag = "debug-output"
+
   def fromXml(xml: Node) = {
-    val names = (xml \ "@names").asBoolean
-    val types = (xml \ "@types").asBoolean
-    DebugOutput(names, types)
+    val names = xml \ "@names" asBoolean
+    val types = xml \ "@types" asBoolean
+    val title = xml \ "title" getAsString
+    val maxWidth = xml \ "@max-width" getAsInt
+
+    apply(names, types, title, maxWidth)
+  }
+  
+  def fromJson(json: JValue) = {
+    val names = json \ "names" asBoolean
+    val types = json \ "types" asBoolean
+    val title = json \ "title" getAsString
+    val maxWidth = json \ "maxWidth" getAsInt
+ 
+    apply(names, types, title, maxWidth)
   }
 }
