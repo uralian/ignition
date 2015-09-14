a7d60f9dd7bb6924fab787335e19253099b14531
Vlad Orzhekhovskiy <vlad@uralian.com>
2015-09-14 10:48:54 -0400

    SelectValues - toXml/fromXml, toJson/fromJson implemented

--- src/main/scala/com/ignition/frame/SelectValues.scala
+++ src/main/scala/com/ignition/frame/SelectValues.scala
@@ -1,10 +1,18 @@
 package com.ignition.frame
 
+import scala.xml.{ Elem, Node }
+
 import org.apache.spark.sql.DataFrame
-import org.apache.spark.sql.types.{DataType, StructType}
+import org.apache.spark.sql.types.{ DataType, StructType }
+import org.json4s.{ JString, JValue }
+import org.json4s.JsonDSL.{ pair2Assoc, seq2jvalue, string2jvalue }
+import org.json4s.jvalue2monadic
 
 import com.ignition.SparkRuntime
 import com.ignition.types.TypeUtils
+import com.ignition.types.TypeUtils.typeForName
+import com.ignition.util.JsonUtils.RichJValue
+import com.ignition.util.XmlUtils.RichNodeSeq
 
 /**
  * Action performed by SelectValues step.
@@ -12,6 +20,8 @@ import com.ignition.types.TypeUtils
 sealed trait SelectAction extends Serializable {
   def apply(df: DataFrame): DataFrame
   def apply(schema: StructType): StructType
+  def toXml: Elem
+  def toJson: JValue
 }
 
 /**
@@ -25,9 +35,13 @@ object SelectAction {
   case class Retain(names: Iterable[String]) extends SelectAction {
     def apply(df: DataFrame): DataFrame = df.select(names map df.col toSeq: _*)
     def apply(schema: StructType): StructType = StructType(names map schema.apply toSeq)
+    def toXml: Elem = <retain>{ names map (n => <field name={ n }/>) }</retain>
+    def toJson: JValue = ("action" -> "retain") ~ ("fields" -> names)
   }
   object Retain {
     def apply(names: String*): Retain = apply(names.toSeq)
+    def fromXml(xml: Node) = Retain(xml \ "field" map (_ \ "@name" asString))
+    def fromJson(json: JValue) = Retain((json \ "fields" asArray) map (_ asString))
   }
 
   /**
@@ -49,10 +63,33 @@ object SelectAction {
       StructType(fields)
     } ensuring (checkDuplicates(_), "Duplicate field names")
 
+    def toXml: Elem = <rename>{ dictionary map (n => <field oldName={ n._1 } newName={ n._2 }/>) }</rename>
+
+    def toJson: JValue = ("action" -> "rename") ~
+      ("fields" -> dictionary.map(t => ("oldName" -> t._1) ~ ("newName" -> t._2)))
+
     private def checkDuplicates(schema: StructType) = schema.fieldNames.toSet.size == schema.fields.size
   }
   object Rename {
     def apply(pairs: (String, String)*): Rename = apply(pairs.toMap)
+    def fromXml(xml: Node) = {
+      val dictionary = xml \ "field" map { node =>
+        val oldName = node \ "@oldName" asString
+        val newName = node \ "@newName" asString
+
+        oldName -> newName
+      }
+      Rename(dictionary.toMap)
+    }
+    def fromJson(json: JValue) = {
+      val dictionary = (json \ "fields" asArray) map { node =>
+        val oldName = node \ "oldName" asString
+        val newName = node \ "newName" asString
+
+        oldName -> newName
+      }
+      Rename(dictionary.toMap)
+    }
   }
 
   /**
@@ -67,9 +104,13 @@ object SelectAction {
       val fields = schema filterNot (f => names.toSeq.contains(f.name))
       StructType(fields)
     }
+    def toXml: Elem = <remove>{ names map (n => <field name={ n }/>) }</remove>
+    def toJson: JValue = ("action" -> "remove") ~ ("fields" -> names)
   }
   object Remove {
     def apply(names: String*): Remove = apply(names.toSeq)
+    def fromXml(xml: Node) = Remove(xml \ "field" map (_ \ "@name" asString))
+    def fromJson(json: JValue) = Remove((json \ "fields" asArray) map (_ asString))
   }
 
   /**
@@ -89,11 +130,47 @@ object SelectAction {
       }
       StructType(fields)
     }
+    def toXml: Elem = <retype>{ dictionary map (n => <field name={ n._1 } type={ n._2.typeName }/>) }</retype>
+    def toJson: JValue = ("action" -> "retype") ~
+      ("fields" -> dictionary.map(t => ("name" -> t._1) ~ ("type" -> t._2.typeName)))
   }
   object Retype {
     def apply(pairs: (String, String)*): Retype = apply(pairs.map {
       case (name, typeName) => (name, TypeUtils.typeForName(typeName))
     }.toMap)
+    def fromXml(xml: Node) = {
+      val dictionary = xml \ "field" map { node =>
+        val name = node \ "@name" asString
+        val dataType = typeForName(node \ "@type" asString)
+
+        name -> dataType
+      }
+      Retype(dictionary.toMap)
+    }
+    def fromJson(json: JValue) = {
+      val dictionary = (json \ "fields" asArray) map { node =>
+        val name = node \ "name" asString
+        val dataType = typeForName(node \ "type" asString)
+
+        name -> dataType
+      }
+      Retype(dictionary.toMap)
+    }
+  }
+
+  def fromXml(xml: Node) = xml match {
+    case <rename>{ _* }</rename> => Rename.fromXml(xml)
+    case <remove>{ _* }</remove> => Remove.fromXml(xml)
+    case <retain>{ _* }</retain> => Retain.fromXml(xml)
+    case <retype>{ _* }</retype> => Retype.fromXml(xml)
+  }
+
+  def fromJson(json: JValue) = json \ "action" match {
+    case JString("rename") => Rename.fromJson(json)
+    case JString("remove") => Remove.fromJson(json)
+    case JString("retain") => Retain.fromJson(json)
+    case JString("retype") => Retype.fromJson(json)
+    case x => throw new IllegalArgumentException(s"Unknown action: $x")
   }
 }
 
@@ -103,6 +180,7 @@ object SelectAction {
  * @author Vlad Orzhekhovskiy
  */
 case class SelectValues(actions: Iterable[SelectAction]) extends FrameTransformer {
+  import SelectValues._
   import SelectAction._
 
   def retain(names: String*) = copy(actions = actions.toSeq :+ Retain(names.toSeq))
@@ -119,11 +197,21 @@ case class SelectValues(actions: Iterable[SelectAction]) extends FrameTransforme
 
   protected def computeSchema(inSchema: StructType)(implicit runtime: SparkRuntime): StructType =
     actions.foldLeft(inSchema)((schema, action) => action(schema))
+
+  def toXml: Elem = <node>{ actions map (_.toXml) }</node>.copy(label = tag)
+
+  def toJson: JValue = ("tag" -> tag) ~ ("actions" -> actions.map(_.toJson))
 }
 
 /**
  * Select Values companion object.
  */
 object SelectValues {
+  val tag = "select-values"
+
   def apply(actions: SelectAction*): SelectValues = apply(actions.toSeq)
+
+  def fromXml(xml: Node) = apply(xml.child map SelectAction.fromXml)
+
+  def fromJson(json: JValue) = apply((json \ "actions" asArray) map SelectAction.fromJson)
 }
