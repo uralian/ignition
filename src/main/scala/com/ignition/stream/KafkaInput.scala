fa12ff4ee2bfd54a82b02418ede1987fc1f76e37
Vlad Orzhekhovskiy <vlad@uralian.com>
2015-10-13 07:01:35 -0400

    Fix stream.KafkaInput step

--- src/main/scala/com/ignition/stream/KafkaInput.scala
+++ src/main/scala/com/ignition/stream/KafkaInput.scala
@@ -1,12 +1,18 @@
 package com.ignition.stream
 
+import scala.xml.{ Elem, Node }
+
 import org.apache.spark.sql.Row
 import org.apache.spark.sql.catalyst.expressions.GenericRowWithSchema
-import org.apache.spark.sql.types.StructType
 import org.apache.spark.streaming.kafka.KafkaUtils
+import org.json4s.JValue
+import org.json4s.JsonDSL._
+import org.json4s.jvalue2monadic
 
 import com.ignition.SparkRuntime
 import com.ignition.types.{ fieldToStructType, string }
+import com.ignition.util.JsonUtils.RichJValue
+import com.ignition.util.XmlUtils.RichNodeSeq
 
 import kafka.serializer.StringDecoder
 
@@ -17,6 +23,8 @@ import kafka.serializer.StringDecoder
  */
 case class KafkaInput(brokers: Iterable[String], topics: Iterable[String],
                       kafkaProperties: Map[String, String] = Map.empty, field: String = "payload") extends StreamProducer {
+
+  import KafkaInput._
 
   def brokers(hosts: String*): KafkaInput = copy(brokers = hosts)
   def brokers(hosts: String): KafkaInput = copy(brokers = hosts.split(",\\s*"))
@@ -38,15 +46,70 @@ case class KafkaInput(brokers: Iterable[String], topics: Iterable[String],
     }
   }
 
-  protected def computeSchema(implicit runtime: SparkRuntime): StructType = schema
+  def toXml: Elem =
+    <node>
+      <field>{ field }</field>
+      <brokers>
+        { brokers map (b => <broker>{ b }</broker>) }
+      </brokers>
+      <topics>
+        { topics map (t => <topic>{ t }</topic>) }
+      </topics>
+      {
+        if (!kafkaProperties.isEmpty)
+          <kafkaProperties>
+            {
+              kafkaProperties map {
+                case (name, value) => <property name={ name }>{ value }</property>
+              }
+            }
+          </kafkaProperties>
+      }
+    </node>.copy(label = tag)
 
-  def toXml: scala.xml.Elem = ???
-  def toJson: org.json4s.JValue = ???
+  def toJson: JValue = {
+    val props = if (kafkaProperties.isEmpty) None else Some(kafkaProperties.map {
+      case (name, value) => ("name" -> name) ~ ("value" -> value)
+    })
+    ("tag" -> tag) ~ ("field" -> field) ~ ("brokers" -> brokers) ~ ("topics" -> topics) ~ ("kafkaProperties" -> props)
+  }
 }
 
 /**
  * Kafka Input companion object.
  */
 object KafkaInput {
+  val tag = "stream-kafka-input"
+
   def apply(): KafkaInput = apply(Nil, Nil)
+
+  def fromXml(xml: Node) = {
+    val field = xml \ "field" asString
+    val brokers = xml \\ "broker" map (_.asString)
+    val topics = xml \\ "topic" map (_.asString)
+
+    val properties = xml \ "kafkaProperties" \ "property" map { node =>
+      val name = node \ "@name" asString
+      val value = node.child.head asString
+
+      name -> value
+    } toMap
+
+    apply(brokers, topics, properties, field)
+  }
+  
+  def fromJson(json: JValue) = {
+    val field = json \ "field" asString
+    val brokers = (json \ "brokers" asArray) map (_.asString)
+    val topics = (json \ "topics" asArray) map (_.asString)
+    
+    val properties = (json \ "kafkaProperties" asArray) map { item =>
+      val name = item \ "name" asString
+      val value = item \ "value" asString
+
+      name -> value
+    } toMap
+    
+    apply(brokers, topics, properties, field)
+  }
 }
--- src/test/scala/com/ignition/stream/KafkaInputSpec.scala
+++ src/test/scala/com/ignition/stream/KafkaInputSpec.scala
@@ -1,19 +1,38 @@
-//package com.ignition.stream
-//
-//import org.junit.runner.RunWith
-//import org.specs2.runner.JUnitRunner
-//import com.ignition.types.{ fieldToRichStruct, int, string }
-//import com.ignition.DefaultSparkRuntime
-//import org.apache.spark.streaming.ClockWrapper
-//
-//@RunWith(classOf[JUnitRunner])
-//class KafkaInputSpec extends StreamFlowSpecification {
-//
-//  "KafkaInput" should {
-//    "work" in {
-//      //TODO need to mock up kafka broker here
-//      success
-//    }
-//  }
-//
-//}
+package com.ignition.stream
+
+import org.junit.runner.RunWith
+import org.specs2.runner.JUnitRunner
+import com.ignition.types.{ fieldToRichStruct, int, string }
+import com.ignition.DefaultSparkRuntime
+import org.apache.spark.streaming.ClockWrapper
+
+@RunWith(classOf[JUnitRunner])
+class KafkaInputSpec extends StreamFlowSpecification {
+
+  "KafkaInput" should {
+    "save to/load from xml" in {
+      val ki = KafkaInput(List("broker1", "broker2"), List("topic1"), Map("key" -> "value"), "data")
+      ki.toXml must ==/(
+        <stream-kafka-input>
+          <brokers><broker>broker1</broker><broker>broker2</broker></brokers>
+          <topics><topic>topic1</topic></topics>
+          <field>data</field>
+          <kafkaProperties>
+            <property name="key">value</property>
+          </kafkaProperties>
+        </stream-kafka-input>)
+      KafkaInput.fromXml(ki.toXml) === ki
+    }
+    "save to/load from json" in {
+      import org.json4s.JsonDSL._
+
+      val ki = KafkaInput(List("broker1", "broker2"), List("topic1"), Map("key" -> "value"), "data")
+      ki.toJson === ("tag" -> "stream-kafka-input") ~
+        ("brokers" -> List("broker1", "broker2")) ~
+        ("topics" -> List("topic1")) ~
+        ("field" -> "data") ~
+        ("kafkaProperties" -> List(("name" -> "key") ~ ("value" -> "value")))
+      KafkaInput.fromJson(ki.toJson) === ki
+    }
+  }
+}
