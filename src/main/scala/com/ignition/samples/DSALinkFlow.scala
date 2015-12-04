2c2e69fb820604a27f5cb526b7999bd94b8e522c
Vlad Orzhekhovskiy <vlad@uralian.com>
2015-12-03 19:49:04 -0500

    Implement DSAStreamInput and DSAStreamOutput steps, close #54, close #55

---
+++ src/main/scala/com/ignition/samples/DSALinkFlow.scala
@@ -0,0 +1,33 @@
+package com.ignition.samples
+
+import org.apache.spark.sql.types.DoubleType
+import org.dsa.iot.spark.RichInt
+
+import com.ignition.SparkPlug
+import com.ignition.frame.SQLQuery
+import com.ignition.frame.mllib.Correlation
+import com.ignition.stream.{ DSAStreamInput, DSAStreamOutput, StreamFlow, Window, foreach }
+
+object DSALinkFlow extends App {
+
+  val flow = StreamFlow {
+
+    val dsaIn = DSAStreamInput() %
+      ("/downstream/System/Memory_Usage" -> DoubleType) %
+      ("/downstream/System/CPU_Usage" -> DoubleType)
+
+    val sql = foreach { SQLQuery("select AVG(CPU_Usage) as cpu, AVG(Memory_Usage) as mem from input0") }
+
+    val win = Window(30 seconds)
+
+    val corr = foreach { Correlation() % "cpu" % "mem" }
+
+    val dsaOut = DSAStreamOutput() % ("corr_cpu_mem" -> "/output/mem_cpu_correlation")
+
+    dsaIn --> sql --> win --> corr --> dsaOut
+
+    (dsaOut)
+  }
+
+  SparkPlug.startStreamFlow(flow)
+}
