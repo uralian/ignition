8b4d38695bd26e3db2c88ddbae7987b46cb7825a
Vlad Orzhekhovskiy <vlad@uralian.com>
2016-02-16 10:27:34 -0500

    Comment out RestClient tests until they are moved to integration tests

--- src/test/scala/com/ignition/frame/RestClientSpec.scala
+++ src/test/scala/com/ignition/frame/RestClientSpec.scala
@@ -7,6 +7,8 @@ import com.ignition.types.{ RichStructType, fieldToRichStruct, int, string }
 
 @RunWith(classOf[JUnitRunner])
 class RestClientSpec extends FrameFlowSpecification {
+  sequential
+  
   import HttpMethod._
 
   System.setProperty("apikey", "2a87e6d26f5fd1d507fea7dfbbcf35d8")
@@ -17,49 +19,49 @@ class RestClientSpec extends FrameFlowSpecification {
   //TODO: real calls to be replaced with mocks
 
   "RestClient" should {
-    "return valid result and status" in {
-      System.setProperty("weather_url", "http://api.openweathermap.org/data/2.5/weather")
-      rt.vars("query") = "q"
-      val url = "e{weather_url}?v{query}=$" + "{city},$" + "{country}&APPID=e{apikey}"
-      val client = RestClient(url) result "result" status "status"
-      grid --> client
-
-      assertSchema(schema ~ string("result") ~ int("status"), client, 0)
-      client.output.collect.forall(!_.getString(2).isEmpty)
-      client.output.collect.forall(_.getInt(3) == 200)
-    }
-    "return valid result and headers" in {
-      val url = "http://api.openweathermap.org/data/2.5/weather?q=$" + "{city},$" + "{country}&APPID=e{apikey}"
-      val client = RestClient(url, GET) result "result" noStatus () responseHeaders "headers"
-      grid --> client
-
-      assertSchema(schema ~ string("result") ~ string("headers"), client, 0)
-      client.output.collect.forall(!_.getString(3).isEmpty)
-    }
-    "return failure code for missing key" in {
-      val url = "http://api.openweathermap.org/data/2.5/unknown"
-      val client = RestClient(url, GET, None, Map.empty, None, Some("status"), None)
-      grid --> client
-      
-      assertSchema(schema ~ int("status"), client, 0)
-      client.output.collect.forall(_.getInt(2) == 401)
-    }
-    "return failure code for bad path" in {
-      val url = "http://api.openweathermap.org/data/2.5/unknown?APPID=e{apikey}"
-      val client = RestClient(url, GET, None, Map.empty, None, Some("status"), None)
-      grid --> client
-      
-      assertSchema(schema ~ int("status"), client, 0)
-      client.output.collect.forall(_.getInt(2) != 200)
-    }
-    "fail for unknown host" in {
-      val url = "http://unknown_host_for_ignition_testing.org"
-      val client = RestClient(url, GET, None, Map.empty, None, Some("status"), None)
-      grid --> client
-
-      assertSchema(schema ~ int("status"), client, 0)
-      client.output.collect must throwA[Exception]
-    }
+//    "return valid result and status" in {
+//      System.setProperty("weather_url", "http://api.openweathermap.org/data/2.5/weather")
+//      rt.vars("query") = "q"
+//      val url = "e{weather_url}?v{query}=$" + "{city},$" + "{country}&appid=e{apikey}"
+//      val client = RestClient(url) result "result" status "status"
+//      grid --> client
+//
+//      assertSchema(schema ~ string("result") ~ int("status"), client, 0)
+//      client.output.collect.forall(!_.getString(2).isEmpty)
+//      client.output.collect.forall(_.getInt(3) == 200)
+//    }
+//    "return valid result and headers" in {
+//      val url = "http://api.openweathermap.org/data/2.5/weather?q=$" + "{city},$" + "{country}&APPID=e{apikey}"
+//      val client = RestClient(url, GET) result "result" noStatus () responseHeaders "headers"
+//      grid --> client
+//
+//      assertSchema(schema ~ string("result") ~ string("headers"), client, 0)
+//      client.output.collect.forall(!_.getString(3).isEmpty)
+//    }
+//    "return failure code for missing key" in {
+//      val url = "http://api.openweathermap.org/data/2.5/unknown"
+//      val client = RestClient(url, GET, None, Map.empty, None, Some("status"), None)
+//      grid --> client
+//      
+//      assertSchema(schema ~ int("status"), client, 0)
+//      client.output.collect.forall(_.getInt(2) == 401)
+//    }
+//    "return failure code for bad path" in {
+//      val url = "http://api.openweathermap.org/data/2.5/unknown?APPID=e{apikey}"
+//      val client = RestClient(url, GET, None, Map.empty, None, Some("status"), None)
+//      grid --> client
+//      
+//      assertSchema(schema ~ int("status"), client, 0)
+//      client.output.collect.forall(_.getInt(2) != 200)
+//    }
+//    "fail for unknown host" in {
+//      val url = "http://unknown_host_for_ignition_testing.org"
+//      val client = RestClient(url, GET, None, Map.empty, None, Some("status"), None)
+//      grid --> client
+//
+//      assertSchema(schema ~ int("status"), client, 0)
+//      client.output.collect must throwA[Exception]
+//    }
     "save to/load from xml" in {
       val c1 = RestClient("yahoo.com") method GET result "result" status "status" body "msg"
       c1.toXml must ==/(
